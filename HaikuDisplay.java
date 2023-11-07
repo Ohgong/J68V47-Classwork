08fd38fe514159d5e7c918e7d74b2ce2c19a8e92
30074778 <30074778@nescol.ac.uk>
2023-11-07 14:19:41 +0000

    done

---
+++ HaikuDisplay.java
@@ -0,0 +1,8 @@
+public class HaikuDisplay{
+    public static void main(String[] args){
+        System.out.println("Heat waves scorches land");
+        System.out.println("Crops wilt under blazing sun");
+        System.out.println("Climate change marches on");
+    }
+}
+
